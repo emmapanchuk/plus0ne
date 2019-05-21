662e86f164c3601fcba3a575612ffd0aa86ee77f
Justin Moonie <jus7in.moonie@gmail.com>
2019-05-20 20:38:09 -0700

    In Progress: dynamic text field for iOS and Android specific devices

--- plus0ne_flutter/lib/widgets/chatBot.dart
+++ plus0ne_flutter/lib/widgets/chatBot.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:plus0ne/widgets/stateless/dialogueBox.dart';
+import 'dart:io' show Platform;
 
 class ChatBot extends StatefulWidget{
     ChatBot({Key key}): super(key:key);
@@ -22,35 +24,82 @@ class ChatBotState extends State<ChatBot>{
     }
 
     Widget chatEnvironment(){
-        return IconTheme(
-            data: new IconThemeData(color: Colors.blue),
-            child: new Container(
-                margin: const EdgeInsets.symmetric(horizontal:8.0),
-                child: new Row(
-                    children: <Widget>[
-                        new Flexible(
-                            fit: FlexFit.tight,
-                            flex: 2,
-                            child: new TextField(
-                                decoration: new InputDecoration.collapsed(hintText: "Start typing ..."),
-                                controller: chatController,
-                                onSubmitted: handleSubmit,
+        if(Platform.isIOS){
+            return IconTheme(
+                data: new IconThemeData(color: Colors.blue),
+                child: new Container(
+                    margin: const EdgeInsets.symmetric(horizontal:8.0, vertical: 5.0),
+                    child: new Row(
+                        children: <Widget>[
+                            new Flexible(
+                                fit: FlexFit.tight,
+                                flex: 2,
+                                child: new TextField(
+                                    keyboardAppearance: Brightness.dark,
+                                    maxLines: null,
+                                    decoration: InputDecoration(
+                                        suffixIcon: new IconButton(
+                                            icon: const Icon(Icons.arrow_upward),
+                                            onPressed: ()=> handleSubmit(chatController.text),
+                                        ),
+                                        enabledBorder: const OutlineInputBorder(
+                                            borderSide: const BorderSide(color: Colors.red),
+                                        ),
+                                        border: const OutlineInputBorder(
+                                            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                                            borderSide: BorderSide(color: Colors.red),
+                                        ),
+                                        contentPadding: EdgeInsets.all(16.0),
+                                    ),
+                                    textCapitalization: TextCapitalization.sentences,
+                                    controller: chatController,
+                                    onSubmitted: handleSubmit,
+                                ),
                             ),
-                        ),
-                        new Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                            child: new IconButton(
-                                icon: new Icon(Icons.send),
-
-                                onPressed: ()=> handleSubmit(chatController.text),
-
+                            new Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                                child: new IconButton(
+                                    icon: new Icon(Icons.mic),
+                                    onPressed: null,
+                                ),
                             ),
-                        )
-                    ],
+                        ],
+                    ),
+
                 ),
+            );
+        }
+        else if(Platform.isAndroid){
+            return IconTheme(
+                data: new IconThemeData(color: Colors.blue),
+                child: new Container(
+                    margin: const EdgeInsets.symmetric(horizontal:8.0),
+                    child: new Row(
+                        children: <Widget>[
+                            new Flexible(
+                                fit: FlexFit.tight,
+                                flex: 2,
+                                child: new TextField(
+                                    decoration: new InputDecoration.collapsed(hintText: "Start typing ..."),
+                                    controller: chatController,
+                                    onSubmitted: handleSubmit,
+                                ),
+                            ),
+                            new Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                                child: new IconButton(
+                                    icon: new Icon(Icons.send),
 
-            ),
-        );
+                                    onPressed: ()=> handleSubmit(chatController.text),
+
+                                ),
+                            )
+                        ],
+                    ),
+
+                ),
+            );
+        }
     }
 
     @override
